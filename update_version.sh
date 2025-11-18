788a06f11451ca1008d863b7f489f9157da4d194
liziqi <123>
2025-11-18 00:09:32 -0300

    1118

--- update_version.sh
+++ update_version.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-pkill -f "python3 APRS_Reporter.py"
+pkill -f "python3 RPI_Tracker.py"
 
 i2cset -y 1 0x57 0x06 0x18
 #关闭pi sugar硬件看门狗
@@ -10,7 +10,7 @@ mount -o remount,rw / ; sudo mount -o remount,rw /boot
 git reset --hard
 git pull origin main
 echo "Code pulled on $(date)"
-cp -r * /etc/RPI_APRS
+cp -r * /etc/RPI_Tracker
 echo "Copy to /etc finished"
 
 systemctl restart aprs_reporter.service
